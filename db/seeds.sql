37f3a97356be27c808aafbd6b4401fc23e88ad97
Ryan Compton <ryan.wade.compton@gmail.com>
2020-03-18 17:49:13 -0600

    configure server. configure config and db directory.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("mcdouble", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("impossible", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("cheese-burger", false );
